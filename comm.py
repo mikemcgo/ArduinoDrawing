997a4517db88ddcd97f5e975f8dbd79b0d72e7d3
mikemcgo <mike.mcgookey@gmail.com>
2016-09-11 18:18:01 -0400

    Added mouse tracking

--- comm.py
+++ comm.py
@@ -1,15 +1,23 @@
 import serial
 import time
+import pyautogui
 
 # Open serial connection
 ser = serial.Serial('/dev/cu.usbserial-DN01JC0B')
 
 while True:
-	# Retrieve data to send via usb (only numbers, separated by , or ;)
-	data = input("Data to transmit: ");
+	# Record cursor location every two seconds
+	time.sleep(2);
+	raw = pyautogui.position();
+
+	# Convert data to a string
+	data = str(raw[0]) + "," + str(raw[1]) + ";"
+
+	# Exit if in the top left
+	exit = (0,0)
 
 	# Look for exit condition
-	if data == 'quit':
+	if data == exit:
 		break;
 
 	# Encode the data to byte form
